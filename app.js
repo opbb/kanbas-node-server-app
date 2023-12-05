ad3990c00fa924238c9afb43c433eeca9ab342c5
Oliver Baer-Benson <opbb83@gmail.com>
2023-12-05 01:43:50 -0500

    a6 complete

--- app.js
+++ app.js
@@ -7,9 +7,29 @@ import ModuleRoutes from "./modules/routes.js";
 import UserRoutes from "./users/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import "dotenv/config";
+import session from "express-session";
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
